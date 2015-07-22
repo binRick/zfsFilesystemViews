b1f431977eb5a96ab89498f773758bbf0cb32503
Rick <rick@Ricks-MacBook-Air.local>
2015-07-22 17:00:51 -0600

    i# Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 module.exports = {
-        Snapshot: Servers: {
+    Snapshot: {
+        Servers: {
             List: {
                 key: 'Snapshot_Servers_List',
                 field: 2,
                 ttl: 30,
                 matches: ['tank/Snapshots/[a-z]*.[a-z]/tank$'],
-		unique: true,
+                unique: true,
             }
         },
     },
